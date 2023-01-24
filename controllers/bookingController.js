67170ea92750097bcad2c3ad1f595ab868c0fb49
Serhii Kamets <sergiokamets@gmail.com>
2023-01-24 12:07:37 +0100

    Improve stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -31,7 +31,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/tour-1-cover.jpg`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
         },
         quantity: 1,
